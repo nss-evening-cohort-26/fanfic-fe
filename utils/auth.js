bee39b5fe37278bc198f495358a46e584efb5666
Ryan Shore <mshorecode@gmail.com>
2024-04-23 20:56:55 -0500

    setup backend connection, setup register form, added twcss

--- utils/auth.js
+++ utils/auth.js
@@ -13,7 +13,13 @@ const checkUser = (uid) => new Promise((resolve, reject) => {
       Accept: 'application/json',
     },
   })
-    .then((resp) => resolve(resp.json()))
+    .then((resp) => {
+      if (resp.ok) {
+        resolve(resp.json());
+      } else {
+        resolve({});
+      }
+    })
     .catch(reject);
 });
 
