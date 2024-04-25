081f9e3653c1fddba1ff762cae6279e03b601d6a
Andrew <andrewspurlock@rocketmail.com>
2024-04-24 19:49:25 -0500

    userData API calls created

---
+++ api/userData.js
@@ -0,0 +1,43 @@
+const getSingleUser = (id) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7074/api/users/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateUser = (payload) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7074/api/users/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const createUser = (payload) => new Promise((resolve, reject) => {
+  fetch('https://localhost:7074/api/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getSingleUser,
+  updateUser,
+  createUser,
+};
