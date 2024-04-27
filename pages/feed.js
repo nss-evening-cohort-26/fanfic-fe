61e205b3afe463dce006a7edaa8b163bc07548c4
Andrew <andrewspurlock@rocketmail.com>
2024-04-27 10:38:43 -0500

    feed.js page created

--- pages/feed.js
+++ pages/feed.js
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Button } from 'bootstrap';
-// import { useAuth } from '../utils/context/authContext';
-import { getAllPosts } from '../api/postData';
+// import { useEffect, useState } from 'react';
+// import Link from 'next/link';
+// import { Button } from 'bootstrap';
+// // import { useAuth } from '../utils/context/authContext';
+// import { getAllPosts } from '../api/postData';
 
-function Posts() {
-  const [posts, setPosts] = useState([]);
-  const getAllThePosts = () => {
-    getAllPosts().then(setPosts);
-  };
+// function Posts() {
+//   const [posts, setPosts] = useState([]);
+//   const getAllThePosts = () => {
+//     getAllPosts().then(setPosts);
+//   };
 
-  useEffect(() => {
-    getAllThePosts();
-  }, []);
+//   useEffect(() => {
+//     getAllThePosts();
+//   }, []);
 
-  return (
-    <div className="text-center my-4">
-      <Link href="/post/new" passHref>
-        <Button>New Post</Button>
-      </Link>
-      <div className="d-flex flex-wrap">
-        {/* map over authors here using AuthorCard component */}
-        {posts.map((post) => (
-          <PostCard key={post.id} authorObj={post} onUpdate={getAllThePosts} />
-        ))}
-      </div>
+//   return (
+//     <div className="text-center my-4">
+//       <Link href="/post/new" passHref>
+//         <Button>New Post</Button>
+//       </Link>
+//       <div className="d-flex flex-wrap">
+//         {/* map over authors here using AuthorCard component */}
+//         {posts.map((post) => (
+//           <PostCard key={post.id} authorObj={post} onUpdate={getAllThePosts} />
+//         ))}
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default Posts;
+// export default Posts;
