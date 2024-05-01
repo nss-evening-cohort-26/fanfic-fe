eabf93c656e270405852291fd047cc879c0802c1
Andrew <andrewspurlock@rocketmail.com>
2024-04-30 21:19:16 -0500

    created merged data call and updated view page

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,34 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import getSingleUser from '../../api/userData';
-import { getSingleCategory } from '../../api/categoryData';
-import { getSinglePost } from '../../api/postData';
+import { getPostDetails } from '../../api/postData';
 
 export default function ViewPost() {
   const [postDetails, setPostDetails] = useState({});
-  const [author, setAuthor] = useState({});
-  const [category, setCategory] = useState({});
   const router = useRouter();
-
-  // TODO: grab firebaseKey from url
   const { id } = router.query;
 
   useEffect(() => {
-    getSinglePost(id).then(setPostDetails);
-    getSingleUser(id).then(setAuthor);
-    getSingleCategory(id).then(setCategory);
-  }, [id]);
+    getPostDetails(id).then(setPostDetails);
+  }, []);
 
   return (
     <div className="mt-5 d-flex flex-wrap">
       <div className="text-white ms-5 details">
         <h5>
-          {author.firstName} {author.lastName}
+          {postDetails.author?.firstName} {postDetails.author?.lastName}
         </h5>
-        <p>{postDetails.title}</p>
-        <p>{postDetails.content}</p>
-        <p>{category.label}</p>
+        <p>{postDetails?.title}</p>
+        <p>{postDetails?.content}</p>
+        <p>{postDetails.category?.label}</p>
       </div>
     </div>
   );
