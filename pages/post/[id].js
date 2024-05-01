307c4f1d59110d80cebde832ca63615a71a4e554
Andrew <andrewspurlock@rocketmail.com>
2024-04-30 21:20:26 -0500

    disabled dependency array rule

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -10,6 +10,7 @@ export default function ViewPost() {
 
   useEffect(() => {
     getPostDetails(id).then(setPostDetails);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
